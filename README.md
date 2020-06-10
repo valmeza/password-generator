0e1eab5f9c83f157e44812c5f47558af9d237681
Valeria Meza <valeriameza173@gmail.com>
2020-06-09 21:42:46 -0500

    feat: added basic styling and added the random password generator.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # password-generator
+
+Password Generator
+Create a script which will generate a random password, similar to this example. Allow your user to specify:
+
+Total length
+Number of special characters (including none)
+Number of digits (including none)
+Whether to use upper case, lower case, or both
---
+++ js/index.js
@@ -0,0 +1,22 @@
+"use strict";
+
+console.log("All Connected!");
+
+const randomButton = document.getElementById("generate");
+const copyPassword = document.getElementById("copy-password");
+const randomPassword = document.getElementById("password");
+
+randomButton.addEventListener("click", function(e) {
+    e.preventDefault;
+    let len = 10
+    let random = "";
+    let chars = "*!~#@&/ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+    
+    for(let i = 0; i < len; i++) {
+       random += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+     return randomPassword.innerHTML += random;
+});
+
+
+
